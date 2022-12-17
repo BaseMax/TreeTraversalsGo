7e9fb7a7b3f57a48b662a3a96106d9ceae95da19
Max Base <maxbasecode@gmail.com>
2022-12-17 13:47:28 +0100

    comment extra commands

--- TreeTraversals.go
+++ TreeTraversals.go
@@ -63,9 +63,9 @@ func main() {
 		},
 	}
 
-	// Print the tree
-	fmt.Println("Tree:")
-	tree.Print()
+	// // Print the tree
+	// fmt.Println("Tree:")
+	// tree.Print()
 
 	// Print the tree in pre-order
 	fmt.Println("PreOrder:")
@@ -87,90 +87,107 @@ func main() {
 	fmt.Println("ReverseLevelOrder:")
 	tree.ReverseLevelOrder()
 
-	// Print the tree in spiral-order
-	fmt.Println("SpiralOrder:")
-	tree.SpiralOrder()
+	// // Print the tree in spiral-order
+	// fmt.Println("SpiralOrder:")
+	// tree.SpiralOrder()
 
-	// Print the tree in reverse-spiral-order
-	fmt.Println("ReverseSpiralOrder:")
-	tree.ReverseSpiralOrder()
+	// // Print the tree in reverse-spiral-order
+	// fmt.Println("ReverseSpiralOrder:")
+	// tree.ReverseSpiralOrder()
 
-	// Print the tree in zigzag-order
-	fmt.Println("ZigZagOrder:")
-	tree.ZigZagOrder()
+	// // Print the tree in zigzag-order
+	// fmt.Println("ZigZagOrder:")
+	// tree.ZigZagOrder()
 
-	// Print the tree in reverse-zigzag-order
-	fmt.Println("ReverseZigZagOrder:")
-	tree.ReverseZigZagOrder()
+	// // Print the tree in reverse-zigzag-order
+	// fmt.Println("ReverseZigZagOrder:")
+	// tree.ReverseZigZagOrder()
 
-	// Print the tree in vertical-order
-	fmt.Println("VerticalOrder:")
-	tree.VerticalOrder()
+	// // Print the tree in vertical-order
+	// fmt.Println("VerticalOrder:")
+	// tree.VerticalOrder()
 
-	// Print the tree in reverse-vertical-order
-	fmt.Println("ReverseVerticalOrder:")
-	tree.ReverseVerticalOrder()
+	// // Print the tree in reverse-vertical-order
+	// fmt.Println("ReverseVerticalOrder:")
+	// tree.ReverseVerticalOrder()
 
-	// Print the tree in diagonal-order
-	fmt.Println("DiagonalOrder:")
-	tree.DiagonalOrder()
+	// // Print the tree in diagonal-order
+	// fmt.Println("DiagonalOrder:")
+	// tree.DiagonalOrder()
 
-	// Print the tree in reverse-diagonal-order
-	fmt.Println("ReverseDiagonalOrder:")
-	tree.ReverseDiagonalOrder()
+	// // Print the tree in reverse-diagonal-order
+	// fmt.Println("ReverseDiagonalOrder:")
+	// tree.ReverseDiagonalOrder()
 
-	// Print the tree in boundary-order
-	fmt.Println("BoundaryOrder:")
-	tree.BoundaryOrder()
+	// // Print the tree in boundary-order
+	// fmt.Println("BoundaryOrder:")
+	// tree.BoundaryOrder()
 
-	// Print the tree in reverse-boundary-order
-	tree.ReverseBoundaryOrder()
+	// // Print the tree in reverse-boundary-order
+	// fmt.Println("ReverseBoundaryOrder:")
+	// tree.ReverseBoundaryOrder()
 
-	// Print the tree in left-boundary-order
-	tree.LeftBoundaryOrder()
+	// // Print the tree in left-boundary-order
+	// fmt.Println("LeftBoundaryOrder:")
+	// tree.LeftBoundaryOrder()
 
-	// Print the tree in right-boundary-order
-	tree.RightBoundaryOrder()
+	// // Print the tree in right-boundary-order
+	// fmt.Println("RightBoundaryOrder:")
+	// tree.RightBoundaryOrder()
 
-	// Print the tree in reverse-left-boundary-order
-	tree.ReverseLeftBoundaryOrder()
+	// // Print the tree in reverse-left-boundary-order
+	// fmt.Println("ReverseLeftBoundaryOrder:")
+	// tree.ReverseLeftBoundaryOrder()
 
-	// Print the tree in reverse-right-boundary-order
-	tree.ReverseRightBoundaryOrder()
+	// // Print the tree in reverse-right-boundary-order
+	// fmt.Println("ReverseRightBoundaryOrder:")
+	// tree.ReverseRightBoundaryOrder()
 
-	// Print the tree in left-view-order
-	tree.LeftViewOrder()
+	// // Print the tree in left-view-order
+	// fmt.Println("LeftViewOrder:")
+	// tree.LeftViewOrder()
 
-	// Print the tree in right-view-order
-	tree.RightViewOrder()
+	// // Print the tree in right-view-order
+	// fmt.Println("RightViewOrder:")
+	// tree.RightViewOrder()
 
-	// Print the tree in reverse-left-view-order
-	tree.ReverseLeftViewOrder()
+	// // Print the tree in reverse-left-view-order
+	// fmt.Println("ReverseLeftViewOrder:")
+	// tree.ReverseLeftViewOrder()
 
-	// Print the tree in reverse-right-view-order
-	tree.ReverseRightViewOrder()
+	// // Print the tree in reverse-right-view-order
+	// fmt.Println("ReverseRightViewOrder:")
+	// tree.ReverseRightViewOrder()
 
-	// Print the tree in top-view-order
-	tree.TopViewOrder()
+	// // Print the tree in top-view-order
+	// fmt.Println("TopViewOrder:")
+	// tree.TopViewOrder()
 
-	// Print the tree in bottom-view-order
-	tree.BottomViewOrder()
+	// // Print the tree in bottom-view-order
+	// fmt.Println("BottomViewOrder:")
+	// tree.BottomViewOrder()
 
-	// Print the tree in reverse-top-view-order
-	tree.ReverseTopViewOrder()
+	// // Print the tree in reverse-top-view-order
+	// fmt.Println("ReverseTopViewOrder:")
+	// tree.ReverseTopViewOrder()
 
-	// Print the tree in reverse-bottom-view-order
-	tree.ReverseBottomViewOrder()
+	// // Print the tree in reverse-bottom-view-order
+	// fmt.Println("ReverseBottomViewOrder:")
+	// tree.ReverseBottomViewOrder()
 
-	// Print the tree in left-cliff-order
-	tree.LeftCliffOrder()
+	// // Print the tree in left-cliff-order
+	// fmt.Println("LeftCliffOrder:")
+	// tree.LeftCliffOrder()
 
-	// Print the tree in right-cliff-order
-	tree.RightCliffOrder()
+	// // Print the tree in right-cliff-order
+	// fmt.Println("RightCliffOrder:")
+	// tree.RightCliffOrder()
 
-	// Print the tree in reverse-left-cliff-order
-	tree.ReverseLeftCliffOrder()
+	// // Print the tree in reverse-left-cliff-order
+	// fmt.Println("ReverseLeftCliffOrder:")
+	// tree.ReverseLeftCliffOrder()
 
-	// Print the tree in reverse-right-cliff-order
-	tree.ReverseRightCliffOrder()
+	// // Print the tree in reverse-right-cliff-order
+	// fmt.Println("ReverseRightCliffOrder:")
+	// tree.ReverseRightCliffOrder()
 }
